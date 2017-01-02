4c0ad89ed8149638091341fdebe051ce03682bcd
AlvaroErrepeBass <alvrod09@ucm.es>
2017-01-02 18:24:42 +0100

    Creado tilemap y hecho logica básica del juego, mejor cambiar el otor por el ninja

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,19 @@
 'use strict';
 
 var PlayScene = require('./play_scene.js');
+var MenuScene = require('./menu_scene.js');
 
 
 var BootScene = {
   preload: function () {
     // load here assets required for the loading screen
+    this.game.load.spritesheet('button', 'images/buttons.png', 168, 70);
+    this.game.load.image('logo', 'images/phaser.png');
     this.game.load.image('preloader_bar', 'images/preloader_bar.png');
   },
 
   create: function () {
-    this.game.state.start('preloader');
+    this.game.state.start('menu');
   }
 };
 
@@ -22,11 +25,27 @@ var PreloaderScene = {
     this.load.setPreloadSprite(this.loadingBar);
 
     // TODO: load here the assets for the game
-    this.game.load.image('logo', 'images/phaser.png');
+    
+    this.load.onLoadStart.add(this.loadStart, this);
+    this.game.load.image('player', 'images/Leon.png');
+    this.game.load.tilemap('tilemap', 'images/map.json', null, Phaser.Tilemap.TILED_JSON);
+    this.game.load.image("tiles", "images/tiles.png");
+    this.load.onLoadComplete.add(this.loadComplete, this);
   },
 
-  create: function () {
+  create: function(){},
+
+  loadStart: function () {
+    //this.game.state.start('play');
+    console.log("Game Assets Loading ...");
+  },
+
+  loadComplete: function () {
     this.game.state.start('play');
+  },
+
+  update: function(){
+    this._loadingBar
   }
 };
 
@@ -36,6 +55,7 @@ window.onload = function () {
 
   game.state.add('boot', BootScene);
   game.state.add('preloader', PreloaderScene);
+  game.state.add('menu', MenuScene);
   game.state.add('play', PlayScene);
 
   game.state.start('boot');
--- src/js/play_scene.js
+++ src/js/play_scene.js
@@ -1,11 +1,66 @@
 'use strict';
 
 var PlayScene = {
+  _speed: 300, //velocidad del player
+  _jumpSpeed: 500, //velocidad de salto
+  _player: {},
+
   create: function () {
-    var logo = this.game.add.sprite(
-      this.game.world.centerX, this.game.world.centerY, 'logo');
-    logo.anchor.setTo(0.5, 0.5);
+      
+      // Añadimos el tilemap y el yileset
+      this.map = this.add.tilemap('tilemap');
+      this.map.addTilesetImage('tiles', 'tiles');
+      // Creamos los layer del tilemap
+      this.groundLayer = this.map.createLayer('GroundLayer');
+      this.death = this.map.createLayer('Death');
+      this.map.setCollisionBetween(1, 5000, true, 'Death');
+      this.map.setCollisionBetween(1, 5000, true, 'GroundLayer');
+      // Creamos el player y configuramos la escena
+      this._player = this.add.sprite(96, 32, 'player');
+      this.configure();
+      // Creamos los botones de accion
+      this.cursors = this.game.input.keyboard.createCursorKeys();
+      this.jumpButton = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+      this.attackButton = this.game.input.keyboard.addKey(Phaser.Keyboard.CONTROL);
+   
+  },
+  update: function(){
+     // Actiualiza la gravedad y el movimiento
+     this._player.body.velocity.x = 0;
+     this._player.body.velocity.y = this._player.body.velocity.y + 2 * 9.8;
+     // Si el jugador choca con el mapa la gravedad ya no le afecta
+     if(this.game.physics.arcade.collide(this._player, this.groundLayer)){
+        this._player.body.gravity.y = 0;     
+     }
+     // Gestion del movimiento
+     if (this.cursors.left.isDown){
+        this._player.body.velocity.x = this._speed*-1;
+     }
+     else if (this.cursors.right.isDown){
+        this._player.body.velocity.x = this._speed;
+     }
+     // Gestion del salto
+     if (this.jumpButton.isDown && this._player.body.onFloor() ){
+        this._player.body.velocity.y = this._jumpSpeed*-1;
+     }
+     
+  },
+  configure: function(){
+     //Start the Arcade Physics systems
+     this.game.world.setBounds(0, 0, 5000, 1120);
+     this.game.physics.startSystem(Phaser.Physics.ARCADE);
+     this.game.stage.backgroundColor = '#a9f0ff';
+     this.game.physics.arcade.enable(this._player);
+        
+     this._player.body.gravity.y = 0;
+     this._player.body.gravity.x = 0;
+     this._player.body.velocity.x = 0;
+     this.camera.follow(this._player);
+  },
+  render: function(){
+     this.game.debug.cameraInfo(this.camera, 32, 32);
   }
+  
 };
 
 module.exports = PlayScene;
